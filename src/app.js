d6568b22ca373b6aba933e1b2f9478b9c4f47289
9ian9 <108910700+9ian9@users.noreply.github.com>
2024-11-16 05:02:05 +0700

    change language hompage

--- src/app.js
+++ src/app.js
@@ -6,17 +6,20 @@ let stream = require('./ws/stream');
 let path = require('path');
 let favicon = require('serve-favicon');
 
+// Cấu hình Express để phục vụ các file trong thư mục 'locales'
+app.use('/locales', express.static(path.join(__dirname, 'locales')));
+
+// Cấu hình favicon và các file static khác
 app.use(favicon(path.join(__dirname, 'favicon.ico')));
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
-
 
 io.of('/stream').on('connection', stream);
 
-// server.listen( 3000 );
+// Khởi động server
 server.listen(3000, () => {
     console.log('Server is running on http://localhost:3000');
 });
